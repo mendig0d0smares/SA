778ce342895069b4d0b8151829ebc642f3dab0b1
caldart <pedro_warmling@estudante.sesisenai.org.br>
2025-12-04 08:35:24 -0300

    Add files via upload

--- page1.js
+++ page1.js
@@ -1,60 +1,64 @@
 
 function calcularOhm() {
-    let R = Number(document.getElementById('R').value)
-    let I = Number(document.getElementById('I').value)
-    let V = Number(document.getElementById('V').value)
-    let res = document.getElementById('res')
+    let R = document.getElementById("R").value;
+    let I = document.getElementById("I").value;
+    let V = document.getElementById("V").value;
+    let res = document.getElementById("res");
 
-    res.innerHTML = ` `
-
-    if (R === 0 && I > 0 && V > 0) {
-        R = V / I
-        res.innerHTML += `resistencia = ${R}Ω`
+    res.innerHTML = "";
+    
+    // calcular R
+    if (R === "" && I !== "" && V !== "") {
+        let resistencia = Number(V) / Number(I);
+        res.innerHTML = `Resistência = ${resistencia} Ω`;
     }
-
-    else if (I === 0 && R > 0 && V > 0) {
-        I = V / R
-        res.innerHTML += `corrente = ${I}A`
+    // calcular I
+    else if (I === "" && R !== "" && V !== "") {
+        let corrente = Number(V) / Number(R);
+        res.innerHTML = `Corrente = ${corrente} A`;
     }
-
-    else if (V === 0 && I > 0 && R > 0) {
-        V = I * R
-        res.innerHTML += `voltagem = ${V}W`
-
-    } else {
-        res.innerHTML += `preencha APENAS 2 VALORES e deixe em branco o valor em que deseja calcular`
+    // calcular V
+    else if (V === "" && I !== "" && R !== "") {
+        let voltagem = Number(I) * Number(R);
+        res.innerHTML = `Voltagem = ${voltagem} V`;
+    }
+    else {
+        res.innerHTML = "Preencha APENAS 2 campos e deixe o terceiro vazio.";
     }
 }
 
 
-        function calcular_potencia() {
-            let res = document.getElementById("res2");
-        let R = Number(document.getElementById("R2").value);
-        let V = Number(document.getElementById("V2").value);
-        let I = Number(document.getElementById("I2").value);
-        let P = 0
+function calcular_potencia() {
+    let res = document.getElementById("res2");
+    
+    let R = document.getElementById("R2").value;
+    let V = document.getElementById("V2").value;
+    let I = document.getElementById("I2").value;
 
-        res.innerHTML = ` `
+    res.innerHTML = "";
 
-        if (R === 0 && I > 0 && V > 0) {
-            P = V * I;
-            res.innerHTML += `Potência = ${P}W`
-        }
+    // Cálculo com campos vazios detectados corretamente
 
-        else if (I === 0 && R > 0 && V > 0) {
-            P = (V * V) / R;
-            res.innerHTML += `Potência = ${P}W`
-        }
-
-        else if (V === 0 && I > 0 && R > 0) {
-            P = (I * I) * R;
-            res.innerHTML += `Potência = ${P} W`
-        }
-
-        else {
-            res.innerHTML += `preencha APENAS 2 VALORES e deixe em branco o valor em que deseja calcular`
-        }
-        console.log(P)
+    // P = V * I
+    if (R === "" && V !== "" && I !== "") {
+        let P = Number(V) * Number(I);
+        res.innerHTML = `Potência = ${P} W`;
     }
 
+    // P = V² / R
+    else if (I === "" && V !== "" && R !== "") {
+        let P = (Number(V) * Number(V)) / Number(R);
+        res.innerHTML = `Potência = ${P} W`;
+    }
+
+    // P = I² * R
+    else if (V === "" && I !== "" && R !== "") {
+        let P = (Number(I) * Number(I)) * Number(R);
+        res.innerHTML = `Potência = ${P} W`;
+    }
+
+    else {
+        res.innerHTML = "Preencha APENAS 2 campos e deixe o terceiro vazio.";
+    }
+}
 
